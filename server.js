d1f957edad1278ff8ff06c38e0f951f78c330479
Juan Ignacio Muñoz <juanignacio.munoz.ov@gmail.com>
2024-05-05 19:53:48 -0300

    Modificacion port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const MQTT = require('mqtt');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Opciones de conexión para MQTT sobre TLS
 const options = {
